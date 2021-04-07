0ed92abfa548eacdfa88448db0284d710313beea
Carlos Quiros <carlosquiros12@gmail.com>
2021-04-07 14:36:47 -0500

    cambios menores

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import session from 'express-session'
 import fertilizationRoutes from './src/routes/fertilizationRoutes.js'
 import fumigationRoutes from './src/routes/fumigationRoutes.js'
 import inventoryRoutes from './src/routes/inventoryRoutes.js'
@@ -16,8 +18,15 @@ app.set('port', process.env.PORT || 3001)
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-
+//Config
+dotenv.config({path: './.env'});
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
 
 //Routes
 app.get('/', (req,res)=>{
--- frontend/src/components/FormLogin.jsx
+++ frontend/src/components/FormLogin.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import useStyles from "../css/FormLoginCss.js";
+import React, { useState } from "react";
+import useStyles from "../styles/FormLoginStyle.js";
 import {
   Avatar,
   Button,
@@ -8,81 +8,101 @@ import {
   Link,
   Paper,
   Box,
-  Grid
+  Grid,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import {MuiThemeProvider} from "@material-ui/core/styles"
-import palette from "../css/palette.js"
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import palette from "../styles/PaletteColors.js";
+import { useForm } from "react-hook-form";
 
 const FormLogin = () => {
-  
   //`http://localhost:3001/fertilization`
+
+  const [username, setusername] = useState();
+  const [password, setpassword] = useState(); 
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setusername(data.username)
+  };
+  
 
   const styles = useStyles();
   const theme = palette;
 
   return (
-      <MuiThemeProvider theme={theme}>
-
-      
-    <Grid container component="main" className={styles.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={styles.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={styles.paper}>
-          <Avatar className={styles.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login HassParadisse
-          </Typography>
-          <form className={styles.form} noValidate action='http://localhost:3001/fertilization' method='get'>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="CC"
-              label="Ingrese por favor su cedula"
-              name="CC"
-              autoComplete="Cedula"
-              autoFocus
-              color='secondary'
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Contraseña"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              color='secondary'
-            />
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className={styles.submit}
+    <MuiThemeProvider theme={theme}>
+      <Grid container component="main" className={styles.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={styles.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={styles.paper}>
+            <Avatar className={styles.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login HassParadisse
+            </Typography>
+            <form
+              className={styles.form}
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
             >
-              Iniciar sesion
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2" className={styles.link}>
-                  ¿Olvidaste tu contraseña?
-                </Link>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Ingrese por favor su cedula"
+                name="username"
+                autoComplete="Cedula"
+                autoFocus
+                color="secondary"
+                inputRef ={register}
+              />
+              
+
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Contraseña"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                color="secondary"
+                inputRef={register}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                className={styles.submit}
+              >
+                Iniciar sesion
+              </Button>
+              
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2" className={styles.link}>
+                    ¿Olvidaste tu contraseña?
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <Box mt={5}></Box>
-          </form>
-        </div>
+              <Box mt={5}></Box>
+            </form>
+
+           
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
     </MuiThemeProvider>
   );
 };
