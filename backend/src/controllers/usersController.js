87df358c30f5ab2e3c73eef05efaf7cf8ad7165c
Carlos Quiros <carlosquiros12@gmail.com>
2021-04-14 17:19:36 -0500

    Carlos Quiros

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -14,13 +14,17 @@ export const logInUser = (req, res) => {
         res.send({ err: err });
       }
       if (result.length > 0) {
-        bcrypt.compare(password, result[0].pass,(error, response)=>{
-          if(response){
-            res.send(result)
-          }else{
+        bcrypt.compare(password, result[0].pass, (error, response) => {
+          if (response) {
+            const nombre = result[0].nombre;
+            const apellido = result[0].apellido;
+            const token = null;
+            const user = {nombre,apellido,token}
+            res.send(user);
+          } else {
             res.send({ message: "El usuario o contraseña son incorrectos" });
           }
-        })
+        });
       } else {
         res.send({ message: "El usuario no existe" });
       }
